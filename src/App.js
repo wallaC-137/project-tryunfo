23f8de31f1dd58a3f0b245476b7f8fecc92b0479
Wallace <wallace_031@hotmail.com>
2022-12-02 20:22:24 -0300

    5. Faça a validação do botão de Salvar no formulário

--- src/App.js
+++ src/App.js
@@ -17,15 +17,50 @@ class App extends React.Component {
     cardImage: '',
     cardRare: '',
     cardTrunfo: false,
+    isSaveButtonDisabled: true,
   };
 
   onInputChange({ target }) {
     const { name, value, type, checked } = target;
-    console.log(name);
     this.setState({
       [name]: type === 'checkbox' ? checked : value,
-    });
+    }, this.btnDisabled);
   }
+
+  btnDisabled = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+
+    const testOne = cardName.length > 0
+    && cardDescription.length > 0
+    && cardImage.length > 0;
+
+    const equal = 210;
+    const testTwo = Number(cardAttr1)
+      + Number(cardAttr2)
+      + Number(cardAttr3) <= equal;
+
+    const smaller = 90;
+    const attr1 = Number(cardAttr1) <= smaller && Number(cardAttr1) >= 0;
+    const attr2 = Number(cardAttr2) <= smaller && Number(cardAttr2) >= 0;
+    const attr3 = Number(cardAttr3) <= smaller && Number(cardAttr3) >= 0;
+    const attrResult = attr1 && attr2 && attr3;
+    this.setState({
+      isSaveButtonDisabled: !(testOne && testTwo && attrResult),
+    });
+  };
+
+  // onSaveButtonClick = (param) => {
+  //   param.preventDefault();
+  //   const { cardName, cardImage, cardDescription,
+  //   cardAttr1, cardAttr2, cardAttr3, cardRare, cardTrunfo } = this.state;
+  // };
 
   render() {
     const {
@@ -37,7 +72,9 @@ class App extends React.Component {
       cardImage,
       cardRare,
       cardTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
+
     return (
       <div>
         <h1>Tryunfo</h1>
@@ -51,6 +88,8 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           onInputChange={ this.onInputChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          // onSaveButtonClick={ this.onSaveButtonClick }
         />
         <Card
           cardName={ cardName }
